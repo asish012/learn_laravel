998d8d298acfd1308c3adfc4ad36ebb13c8fac2a
Biswas <a.biswas@CPMM02C480XMD6M.cpg.int>
2020-12-20 09:54:32 +0100

    api to show person form db

--- peoplefinder/database/seeders/PersonSeeder.php
+++ peoplefinder/database/seeders/PersonSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PersonSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(PersonSeeder::class);
+        \App\Models\Person::factory(15)->create();
     }
 }
